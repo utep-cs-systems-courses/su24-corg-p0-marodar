89e9228266db525a469cc1d812bb7d30fdbc9c49
marodarte6 <marodarte6@miners.utep.edu>
2024-06-17 19:45:53 -0600

    Arrow Implementation

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,29 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+//Prints an arrow
+void print_arrow(int leftCol, int size)
+{
+  for(int row=1; row<=size; row++){
+    int minCol = leftCol + size - row+1, maxCol= leftCol + size + row-1;
+    int col;
+    for(col=1; col<minCol; col++) putchar(' ');
+    for(    ; col<=maxCol; col++) putchar('*');
+    putchar('\n');
+  }
+
+  int arrowWidth = (size*2)+1;
+  for(int i=0; i<2; i++){
+    for(int j=0; j<leftCol+2; j++){
+      putchar(' ');
+    }
+    for(int k=0; k<arrowWidth-6; k++){
+      putchar('*');
+    }
+    putchar('\n');
+  }
+}
+
+
+
+
